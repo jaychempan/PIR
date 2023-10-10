a8489490d7580e2242a80488c6e4ab72d21aefa0
jaychempan <1241762712@qq.com>
2023-10-10 14:37:06 +0800

    remove read_rsdata

--- utils/read_together.py
+++
@@ -1,7 +0,0 @@
-from read_dataset import *
-from loc_bbox import *
-
-rank = int(input("Please enter the number of images you want to query in the data set: \n"))
-img_id = get_img_id(root='/data/pjc/CMR-Pro/data', dataset_name='coco', split='train', rank=rank - 1)
-read_dataset(root='/data/pjc/data', dataset_name='coco', split='train', img_id=img_id)
-loc_bbox(coordinate_path='/data/pjc/CMR-Pro/ITR/bbox_data/coco_precomp/train_ims_bbx.npy', rank=rank - 1)
